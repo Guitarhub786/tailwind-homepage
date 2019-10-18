eecbfa6293f82b7c21749be42382223e3e8fd842
Guitarhub786 <codewithdrawal@gmail.com>
2019-10-18 04:55:06 +0100

    Dark Theme

--- FCA6-ConfirmEnding/script.js
+++ FCA6-ConfirmEnding/script.js
@@ -11,6 +11,10 @@ var attempt = 0;
 
 setTimeout(loadQuestion, 100);
 
+// click btn-auto at start
+document.getElementById("btn-auto").click();
+document.getElementById("run_button").style.color = "#999";
+
 
 run_button.addEventListener("click", function () {
   attempt++;
@@ -33,6 +37,7 @@ run_button.addEventListener("click", function () {
 });
 
 //============== ACE EDITOR ==============
+// ACE 1
 function update() {
 
   var idoc = document.getElementById("iframe").contentWindow.document;
@@ -53,17 +58,18 @@ function setupEditor() {
     `
     <!DOCTYPE html><html><body>
         <h2 id="idOutput">Output...</h2>
-        <h2 id="idDone" onclick="myFunction()" style='color: red' >Waiting ...</h2>
+        <h2 id="idDone" onclick="myFunction()" style='color: crimson' >Waiting ...</h2>
     <script>
         
         document.getElementById("idOutput").innerHTML = x;  
         
         if (x === true) {
-          document.body.style.backgroundColor = "#FFFF64";
+          document.body.style.backgroundColor = "#FFFF66";
           document.getElementById("idDone").style.color = "green"; 
           document.getElementById("idDone").innerHTML = '&#128504; Done!!!'; 
         } else {
-          document.body.style.backgroundColor = "white";
+          document.body.style.backgroundColor = "#282828"; 
+          document.body.style.color = "darkturquoise";
         }
     </script></body></html>
     `,
@@ -74,7 +80,7 @@ function setupEditor() {
     update();
   });
 
-  editor.focus();
+  // editor.focus();
 
   editor.setOptions({
     fontSize: "12pt",
@@ -89,10 +95,8 @@ function setupEditor() {
   editor.setBehavioursEnabled(false);
 }
 
-setupEditor();
-update();
-
 //==================================================
+// ACE 2
 
 function update2() {
 
@@ -127,11 +131,11 @@ function setupEditor2() {
   editor2.setValue(
     // === SET TEMPLATE HERE ===
     `
-function confirmEnding(str, target) {
-  return str;
-}
-
-x = confirmEnding("Abstraction", "action");
+  function confirmEnding(str, target) {
+    return str;
+  }
+  
+  x = confirmEnding("Abstraction", "action");
 `,
     1
   ); //1 = moves cursor to end
@@ -155,6 +159,53 @@ x = confirmEnding("Abstraction", "action");
   editor2.setBehavioursEnabled(false);
 }
 
+//==================================================
+// ACE 3
+
+function setupEditor3() {
+  window.editor3 = ace.edit("editor3");
+  editor3.setTheme("ace/theme/monokai");
+  // editor3.getSession().setMode("ace/mode/javascript");
+  editor3.getSession().setTabSize(0);
+  editor3.getSession().setUseWrapMode(true);
+
+  document.getElementById('editor3').style.fontSize = '16px';
+  document.getElementById('editor3').style.color = 'cyan';
+  editor3.setValue(
+    // === SET x = result HERE ===
+    `
+Return the provided string with the first letter of each word capitalized. Make sure the rest of the word is in lower case.
+
+For the purpose of this exercise, you should also capitalize connecting words like "the" and "of".
+
+titleCase("No need to SHOUT!") should return "No Need To Shout!"
+`,
+    1
+  ); //1 = moves cursor to end
+
+  editor3.getSession().on("change", function () {
+    update();
+  });
+
+  // editor3.focus();
+
+  editor3.setOptions({
+    fontSize: "12pt",
+    showLineNumbers: false,
+    showGutter: false,
+    vScrollBarAlwaysVisible: true,
+    enableBasicAutocompletion: false,
+    enableLiveAutocompletion: false
+  });
+
+  editor3.setShowPrintMargin(false);
+  editor3.setBehavioursEnabled(false);
+  editor3.setReadOnly(true);
+}
+
+//==================================================
+// Load Questions and Answers
+
 function loadQuestion() {
   document.getElementById("myTextarea").style.color = "green";
   document.getElementById("myTextarea").value = displayQuestion;
@@ -167,11 +218,41 @@ function loadAnswer() {
     document.getElementById("btn-answer").innerText = "Hide Answer";
     document.getElementById("myTextarea").style.color = "blue";
     document.getElementById("myTextarea").value = displayAnswer;
+    // .value = displayQuestion + displayAnswer;
+
+    // hide myTextarea and to only show ACE answer
+    document.getElementById("myTextarea").style.display = "none";
+    document.getElementById('editor3').style.width = '33.7%';
+
+    // document.getElementById('editor3').style.fontSize = '15px';
+    document.getElementById('editor3').style.color = '#DCDCDC';
+    // // editor3.getSession().setUseWrapMode(true);
+
+    window.editor3 = ace.edit("editor3");
+    editor3.setTheme("ace/theme/monokai");
+    editor3.getSession().setMode("ace/mode/javascript");
+    editor3.getSession().setTabSize(0);
+    editor3.getSession().setUseWrapMode(true);
+
+    editor3.setValue(displayAnswer);
+    editor3.insert(displayAnswer);
+
     show = false;
   } else {
     document.getElementById("btn-answer").innerText = "Show Answer";
     document.getElementById("myTextarea").style.color = "green";
     document.getElementById("myTextarea").value = displayQuestion;
+
+    // show myTextarea and to only hide ACE answer
+    document.getElementById("myTextarea").style.display = "";
+    document.getElementById('editor3').style.width = '0%';
+
+    document.getElementById('editor3').style.fontSize = '16px';
+    document.getElementById('editor3').style.color = 'cyan';
+    editor3.getSession().setMode();
+    editor3.setValue(displayQuestion);
+    editor3.insert(displayQuestion);
+
     show = true;
   }
 
@@ -183,18 +264,26 @@ function buttonAuto() {
     document.getElementById("btn-auto").innerText = "Auto: off";
     document.getElementById("run_button").setAttribute("disabled", "false");
     document.getElementById("run_button").style.cursor = "not-allowed";
+    document.getElementById("run_button").style.color = "#999";
     autoRun = true;
 
   } else {
     document.getElementById("btn-auto").innerText = "Auto: on";
     document.getElementById("run_button").removeAttribute("disabled");
     document.getElementById("run_button").style.cursor = "pointer";
+    document.getElementById("run_button").style.color = "#DCDCDC";
     autoRun = false;
   }
 }
 
+//==================================================
+// Run Functions 
+setupEditor();
+update();
 setupEditor2();
 update2();
+setupEditor3();
+
 
 // === input QUESTION here ===
 var displayQuestion =
@@ -210,21 +299,24 @@ confirmEnding("Abstraction", "action") should return true.
 var displayAnswer =
   `
 function confirmEnding(str, target) {
-
-  // if (str.endsWith(target)) {
-  //   return true;
-  // }
-  // return false;
-
-  //return str.endsWith(target);
-
-  // if (str.substr(-target.length) === target) {
-  //   return true;
-  // }
-  // return false;
-
   return str.substr(-target.length) === target;
 }
 
 confirmEnding("Abstraction", "action");
+
+
+// solution 2
+if (str.endsWith(target)) {
+  return true;
+}
+return false;
+
+return str.endsWith(target);
+
+
+// solution 3
+if (str.substr(-target.length) === target) {
+  return true;
+}
+return false;
 `;
